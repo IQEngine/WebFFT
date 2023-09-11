480439c91ab7c66700325eeaa352a4a1b7bca4c2
777arc <marcll@vt.edu>
2023-09-11 17:13:37 -0400

    switched to using the old kiss wrapper

---
+++ library/kissfft/webfftWrapper.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var kissFFTModule = KissFFTModule({});
+
+var kiss_fft_alloc = kissFFTModule.cwrap("kiss_fft_alloc", "number", ["number", "number", "number", "number"]);
+
+var kiss_fft = kissFFTModule.cwrap("kiss_fft", "void", ["number", "number", "number"]);
+
+var kiss_fft_free = kissFFTModule.cwrap("kiss_fft_free", "void", ["number"]);
+
+class wrappedKissFFT {
+  constructor(size) {
+    this.size = size;
+
+    this.fcfg = kiss_fft_alloc(size, false);
+    this.icfg = kiss_fft_alloc(size, true);
+
+    this.inptr = kissFFTModule._malloc(size * 8 + size * 8);
+    this.outptr = this.inptr + size * 8;
+
+    this.cin = new Float32Array(kissFFTModule.HEAPU8.buffer, this.inptr, size * 2);
+    this.cout = new Float32Array(kissFFTModule.HEAPU8.buffer, this.outptr, size * 2);
+  }
+
+  forward = function (cin) {
+    this.cin.set(cin);
+    kiss_fft(this.fcfg, this.inptr, this.outptr);
+    return new Float32Array(kissFFTModule.HEAPU8.buffer, this.outptr, this.size * 2);
+  };
+
+  inverse = function (cin) {
+    this.cin.set(cpx);
+    kiss_fft(this.icfg, this.inptr, this.outptr);
+    return new Float32Array(kissFFTModule.HEAPU8.buffer, this.outptr, this.size * 2);
+  };
+
+  dispose = function () {
+    kissFFTModule._free(this.inptr);
+    kiss_fft_free(this.fcfg);
+    kiss_fft_free(this.icfg);
+  };
+}
