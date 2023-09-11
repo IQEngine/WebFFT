7d6709426e931a1e38a361fcfe734ae343d0e585
Maheen Contractor <mcontractor@microsoft.com>
2023-09-11 17:03:57 -0400

    added webfftWrapper for indutny for API support

---
+++ library/indutny/webfftWrapper.js
@@ -0,0 +1,13 @@
+class IndutnyFftWrapper {
+    constructor(size) {
+        this.size = size;
+        this.indutnyFft = new FFT_indutny(size);
+        this.outputArr = new Float32Array(2 * size);
+    }
+
+    fft(inputArr) {
+        var { indutnyFft, outputArr } = this;
+        indutnyFft.transform(outputArr, inputArr);
+        return outputArr;
+    }
+}
