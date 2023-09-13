38a2bcc5514d57eaad700063fa335762d18dfe41
777arc <marcll@vt.edu>
2023-09-13 17:46:42 -0400

    unit test for different fft sizes

--- tests/fft.test.js
+++ tests/fft.test.js
@@ -19,34 +19,37 @@ test("available sublibraries", () => {
   expect(availableSubLibraries.length).toBeGreaterThan(1);
 });
 
-test("outputs for all sublibs approx match", () => {
-  const fftsize = 1024;
-  const fft = new webfft(fftsize);
+test("outputs for all sublibs approx match using different fftsizes", () => {
+  const fftsizes = [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 16384];
+  for (let j = 0; j < fftsizes.length; j++) {
+    const fftsize = fftsizes[j];
+    const fft = new webfft(fftsize);
 
-  const availableSubLibraries = fft.availableSubLibraries();
+    const availableSubLibraries = fft.availableSubLibraries();
 
-  const inputArr = new Float32Array(fftsize * 2);
-  for (let i = 0; i < fftsize * 2; i++) {
-    inputArr[i] = i * 1.12312312; // Arbitrary
-  }
-
-  fft.setSubLibrary("indutnyJavascript");
-  let co = fft.fft(inputArr);
-  let goldenTotal = 0;
-  for (let k = 0; k < fftsize; ++k) {
-    goldenTotal += Math.sqrt(co[k * 2] * co[k * 2] + co[k * 2 + 1] * co[k * 2 + 1]);
-  }
-  // goldenTotal will be 9147216.377928967
-
-  // Try each sub-library
-  for (let i = 0; i < availableSubLibraries.length; i++) {
-    fft.setSubLibrary(availableSubLibraries[i]);
-    co = fft.fft(inputArr);
-    let total = 0;
-    for (let k = 0; k < fftsize; ++k) {
-      total += Math.sqrt(co[k * 2] * co[k * 2] + co[k * 2 + 1] * co[k * 2 + 1]);
+    const inputArr = new Float32Array(fftsize * 2);
+    for (let i = 0; i < fftsize * 2; i++) {
+      inputArr[i] = i * 1.12312312; // Arbitrary
     }
-    console.log(total - goldenTotal);
-    expect(Math.abs(total - goldenTotal)).toBeLessThan(0.9);
+
+    fft.setSubLibrary("indutnyJavascript");
+    let co = fft.fft(inputArr);
+    let goldenTotal = 0;
+    for (let k = 0; k < fftsize; ++k) {
+      goldenTotal += Math.sqrt(co[k * 2] * co[k * 2] + co[k * 2 + 1] * co[k * 2 + 1]);
+    }
+    // goldenTotal will be 9147216.377928967
+
+    // Try each sub-library
+    for (let i = 0; i < availableSubLibraries.length; i++) {
+      fft.setSubLibrary(availableSubLibraries[i]);
+      co = fft.fft(inputArr);
+      let total = 0;
+      for (let k = 0; k < fftsize; ++k) {
+        total += Math.sqrt(co[k * 2] * co[k * 2] + co[k * 2 + 1] * co[k * 2 + 1]);
+      }
+      console.log(total - goldenTotal);
+      expect(Math.abs(total - goldenTotal)).toBeLessThan(goldenTotal * 1e-7);
+    }
   }
 });
