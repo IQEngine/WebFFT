a41b373b56cb6845851ea0c386c8c933e6f7e3af
777arc <marcll@vt.edu>
2023-09-13 00:03:23 -0400

    added fastest sublibrary

--- examples/profile.js
+++ examples/profile.js
@@ -6,3 +6,6 @@ const fftsize = 1024;
 const fft = new webfft(fftsize);
 const profileObj = fft.profile(2); // duration to run profile, in seconds
 console.log(profileObj);
+console.log("Fastest sub-library:", profileObj.fastestSubLibrary);
+
+// it will automatically set future .fft() calls to use whichever was fastest
