d282c37ee58237a1d1b83996c21d3353432b421c
Gabriel Nepomuceno <ganepomu@microsoft.com>
2023-09-13 05:18:07 -0700

    Avoid errors when there is n local storage

--- examples/profile.js
+++ examples/profile.js
@@ -8,4 +8,11 @@ const profileObj = fft.profile(2); // duration to run profile, in seconds
 console.log(profileObj);
 console.log("Fastest sub-library:", profileObj.fastestSubLibrary);
 
+// get it from local storage
+if (typeof localStorage !== "undefined") {
+  const profileObjLocal = JSON.parse(localStorage.getItem("webfftProfile"));
+  console.log(profileObjLocal);
+  console.log("Fastest sub-library:", profileObjLocal.fastestSubLibrary);
+}
+
 // it will automatically set future .fft() calls to use whichever was fastest
