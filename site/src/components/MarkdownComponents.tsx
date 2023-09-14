62c34a84a31c5f00d8e53afeadd716e14508b84f
Chad Peters <chadp@microsoft.com>
2023-09-14 12:22:57 -0400

    merge; update about to by responsive; change mdx/about font readable

--- site/src/components/MarkdownComponents.tsx
+++ site/src/components/MarkdownComponents.tsx
@@ -13,7 +13,7 @@ const CustomParagraph: React.FC<CustomParagraphProps> = (props) => {
   });
 
   return hasCodeElement ? (
-    <div {...props} className="text-cyber-text">
+    <div {...props} className="!font-system text-cyber-text">
       {React.Children.map(children, (child) => {
         if (React.isValidElement(child) && child.type === "code") {
           return child; // Simply return the code child without wrapping it again
@@ -21,23 +21,23 @@ const CustomParagraph: React.FC<CustomParagraphProps> = (props) => {
       })}
     </div>
   ) : (
-    <span {...props} className="text-cyber-text">
+    <span {...props} className="!font-system text-cyber-text">
       {children}
     </span>
   );
 };
 
 const components = {
-  h1: (props: any) => <h1 {...props} className="!text-cyber-text-secondary my-4" />,
-  h2: (props: any) => <h2 {...props} className="!text-cyber-text-secondary my-4" />,
-  h3: (props: any) => <h3 {...props} className="!text-cyber-text-secondary my-4" />,
-  h4: (props: any) => <h4 {...props} className="!text-cyber-text-secondary my-4" />,
-  h5: (props: any) => <h5 {...props} className="!text-cyber-text-secondary my-4" />,
-  h6: (props: any) => <h6 {...props} className="!text-cyber-text-secondary my-4" />,
-  ul: (props: any) => <ul {...props} className="!text-cyber-text my-4" />,
-  li: (props: any) => <li {...props} className="!text-cyber-text my-4" />,
+  h1: (props: any) => <h1 {...props} className="!font-system !text-cyber-text-secondary my-4" />,
+  h2: (props: any) => <h2 {...props} className="!font-system !text-cyber-text-secondary my-4" />,
+  h3: (props: any) => <h3 {...props} className="!font-system !text-cyber-text-secondary my-4" />,
+  h4: (props: any) => <h4 {...props} className="!font-system !text-cyber-text-secondary my-4" />,
+  h5: (props: any) => <h5 {...props} className="!font-system !text-cyber-text-secondary my-4" />,
+  h6: (props: any) => <h6 {...props} className="!font-system !text-cyber-text-secondary my-4" />,
+  ul: (props: any) => <ul {...props} className="!font-system !text-cyber-text my-4" />,
+  li: (props: any) => <li {...props} className="!font-system !text-cyber-text my-4" />,
   blockquote: (props: any) => (
-    <blockquote {...props} className="!text-gray-600 italic my-4 border-l-4 border-gray-500 pl-4" />
+    <blockquote {...props} className="!font-system !text-gray-600 italic my-4 border-l-4 border-gray-500 pl-4" />
   ),
   code: ({ className, children }: any) => {
     const match = /language-(\w+)/.exec(className || "");
