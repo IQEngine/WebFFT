9934934f33955f2b1dcf21cee7c954c2a4d5e783
Maheen Contractor <mcontractor@microsoft.com>
2023-09-14 12:08:25 -0400

    cleared benchmark data when when profiling starts

--- site/src/components/ResultsSection.tsx
+++ site/src/components/ResultsSection.tsx
@@ -1,33 +1,47 @@
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { BallTriangle } from "react-loader-spinner";
 
 // Registering the components
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 interface Props {
   benchmarkData: any;
+  loading: boolean;
 }
 
-function ResultsSection({ benchmarkData }: Props) {
+function ResultsSection({ benchmarkData, loading }: Props) {
   const options = {
     responsive: true,
     maintainAspectRatio: false,
     plugins: {
       legend: {
         position: "top" as const,
-        display: false
+        display: false,
       },
       title: {
         display: true,
-        text: "Test Results"
-      }
-    }
+        text: "Test Results",
+      },
+    },
   };
 
   return (
     <section className="mb-6 text-center">
       <div className="max-w-lg max-h-screen mx-auto p-4">
         <h2 className="text-xl">Results</h2>
+
+        {loading && (
+          <BallTriangle
+            height={200}
+            width={200}
+            radius={5}
+            color="#4fa94d"
+            ariaLabel="ball-triangle-loading"
+            wrapperStyle={{ justifyContent: "center" }}
+            visible={true}
+          />
+        )}
 
         {benchmarkData && (
           <section className="mb-6 text-center">
