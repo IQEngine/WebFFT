082c52adfc02e9ec236b56155391b3faba028057
777arc <marcll@vt.edu>
2023-09-14 01:13:22 -0400

    misc tweaks

--- site/src/components/LinksSection.tsx
+++ site/src/components/LinksSection.tsx
@@ -7,7 +7,7 @@ function LinksSection() {
     <section className="mb-6 text-center">
       <div className="flex flex-col md:flex-row md:justify-center gap-4 rounded-2xl p-4 w-full max-w-lg mx-auto">
         <a
-          href="https://npmjs.com/your-package"
+          href="https://npmjs.com/webfft"
           target="_blank"
           className="rounded-lg font-bold text-center py-4 px-6 border border-cyber-primary bg-cyber-background1 text-cyber-text shadow-button flex items-center justify-center min-w-[200px] min-h-[50px]"
         >
