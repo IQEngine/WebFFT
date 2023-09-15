981444cc7afc6616bf6faf98984b8e637773716a
777arc <marcll@vt.edu>
2023-09-15 12:35:38 -0400

    adding

--- site/src/components/InteractiveSignal.tsx
+++ site/src/components/InteractiveSignal.tsx
@@ -8,31 +8,32 @@ function InteractiveSignal() {
   const [signalStr, setSignalStr] = useState<any>(null);
   const [amplitude, setAmplitude] = useState<number>(20);
   const [frequency, setFrequency] = useState<number>(0.1);
-  //const [update, setUpdate] = useState<number>(0);
-  const [I, setI] = useState<Float32Array>();
+  const [updateRate, setUpdateRate] = useState<number>(0);
+  const [lastUpdate, setLastUpdate] = useState<any>(null);
+  const [update, setUpdate] = useState<number>(0);
+  const [plotData, setPlotData] = useState<Float32Array>();
   const sin = new Float32Array(2 * fftSize);
 
   const webfftInstance = new webfft(fftSize);
 
+  // run profiler once at load
   useEffect(() => {
     webfftInstance.profile(0.5);
   }, []);
 
   useEffect(() => {
-    const x0 = 0; // starting point of curve wrt SVG canvas
     const y0 = 50;
-    let sinString = "M " + x0 + "," + y0;
+    let sinString = "M " + 0 + "," + y0;
 
     sin.fill(0);
     const t = new Date().getTime();
 
     for (let i = 0; i < fftSize; i++) {
-      sinString +=
-        " L " +
-        (x0 + i) +
-        "," +
-        (y0 + 2 * amplitude * Math.sin(frequency * i + t));
-      sin[2 * i] = 2 * amplitude * Math.sin(frequency * i + t);
+      const sample =
+        2 * amplitude * Math.sin(frequency * i + t) +
+        (Math.random() - 0.5) * 50;
+      sinString += " L " + i + "," + (y0 + sample);
+      sin[2 * i] = sample;
     }
 
     setSignalStr(sinString);
@@ -43,21 +44,15 @@ function InteractiveSignal() {
         psd[2 * i] * psd[2 * i] + psd[2 * i + 1] * psd[2 * i + 1],
       );
     }
-    setI(fftshift(mag).slice(fftSize / 2, fftSize));
-  }, [amplitude, frequency]);
-
-  /*
-  function refresh() {
-    setUpdate(update + 1);
-  }
+    setPlotData(fftshift(mag).slice(fftSize / 2, fftSize));
+    setUpdateRate(Math.round(1000 / (performance.now() - lastUpdate)));
+    setLastUpdate(performance.now());
+  }, [amplitude, frequency, update]);
 
   useEffect(() => {
-    const timerId = setInterval(refresh, 50); // in ms
-    return function cleanup() {
-      clearInterval(timerId);
-    };
-  }, []);
-*/
+    setUpdate(Math.random()); // will trigger this useeffect to run again
+  }, [plotData]);
+
   return (
     <section className="mb-6 text-center">
       <div className="w-64 p-4">
@@ -67,15 +62,15 @@ function InteractiveSignal() {
             type="range"
             className="range range-xs range-primary"
             value={String(amplitude)}
-            min={-20}
+            min={0}
             max={20}
-            step={0.01}
+            step={0.1}
             onChange={(e) => {
               setAmplitude(Number(e.target.value));
             }}
           />
         </label>
-
+        <br></br>
         <label className="mb-3" id="formZoom">
           <span className="label-text text-base ">Signal Frequency</span>
           <input
@@ -83,14 +78,13 @@ function InteractiveSignal() {
             className="range range-xs range-primary"
             value={String(frequency)}
             min={0.01}
-            max={Math.PI}
-            step={0.001}
+            max={Math.PI * 0.9}
+            step={0.01}
             onChange={(e) => {
               setFrequency(Number(e.target.value));
             }}
           />
         </label>
-
         <svg width={fftSize} height="100" viewBox="0 0 1024 100">
           <g>
             <path
@@ -102,11 +96,11 @@ function InteractiveSignal() {
             />
           </g>
         </svg>
-
+        <div className="whitespace-nowrap">Update Rate: {updateRate} fps</div>
         <Plot
           data={[
             {
-              y: I,
+              y: plotData,
               type: "scatter",
               name: "I",
             },
@@ -128,7 +122,8 @@ function InteractiveSignal() {
             },
             yaxis: {
               title: "Samples",
-              fixedrange: true,
+              autorange: false,
+              range: [0, 30000],
             },
           }}
           config={{
