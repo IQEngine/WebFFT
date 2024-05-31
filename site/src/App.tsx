b7081880210d22b0cf2737282973b9d4c80b80fc
777arc <marcll@vt.edu>
2024-05-31 01:02:42 -0400

    switch to HashRouter

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Docs from "./components/Docs";
@@ -7,7 +7,7 @@ import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Breadcrumbs />
       <Routes>
         <Route index element={<Home />} errorElement={<NotFound />} />
@@ -15,7 +15,7 @@ function App() {
         <Route path="docs/*" element={<Docs />} errorElement={<NotFound />} />
         <Route path="*" element={<NotFound />} /> // TODO: Make a 404 page
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
